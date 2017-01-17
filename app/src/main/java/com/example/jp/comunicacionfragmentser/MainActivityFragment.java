fee4f0285507dae941b9cc0c076fa07d0e859430
19julio94 <juliopatriciodasilva@gmail.com>
2017-01-17 09:10:24 +0100

    Modificado intent para la comunicacion

--- app/src/main/java/com/example/jp/comunicacionfragmentser/MainActivityFragment.java
+++ app/src/main/java/com/example/jp/comunicacionfragmentser/MainActivityFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -18,28 +19,36 @@ public class MainActivityFragment extends Fragment {
     }
 
     Button boton;
+    TextView texto1;
     static final int PICK_CONTACT_REQUEST = 1;// Codigo de solicitud
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View vista=inflater.inflate(R.layout.fragment_main, container, false);
+        View vista = inflater.inflate(R.layout.fragment_main, container, false);
 
-        boton = (Button)vista.findViewById(R.id.bplay);
+        boton = (Button) vista.findViewById(R.id.bplay);
+        texto1=(TextView)vista.findViewById(R.id.tv1);
 
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent i=new Intent(getActivity(),Main2Activity.class);
+                Intent i = new Intent(getActivity(), Main2Activity.class);
+                i.putExtra("Hola","Activity2");
 
 
-
-                startActivity(i);
+                startActivityForResult(i,PICK_CONTACT_REQUEST);
             }
         });
 
         return vista;
+
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
 
     }
