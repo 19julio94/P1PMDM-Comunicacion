043a2d0261a3d77457d4ac15747999246b632f06
19julio94 <juliopatriciodasilva@gmail.com>
2017-01-17 10:07:24 +0100

    Intent desde fragment1y desde fragment2 ya comunicandose

--- app/src/main/java/com/example/jp/comunicacionfragmentser/Main2ActivityFragment.java
+++ app/src/main/java/com/example/jp/comunicacionfragmentser/Main2ActivityFragment.java
@@ -1,11 +1,16 @@
 package com.example.jp.comunicacionfragmentser;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import static android.app.Activity.RESULT_OK;
+import static com.example.jp.comunicacionfragmentser.MainActivityFragment.CODE;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -20,11 +25,22 @@ public class Main2ActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View vista2=inflater.inflate(R.layout.fragment_main2, container, false);
 
+
+        TextView texto2=(TextView)vista2.findViewById(R.id.tv2);
+        String str=getActivity().getIntent().getStringExtra("llamarActivity1");
+
+        texto2.setText(str);
         Button botonEnviar=(Button)vista2.findViewById(R.id.send);
 
         botonEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent i2=new Intent();
+                i2.putExtra("LlamarActivity2","Hola desde la activity 2");
+
+                getActivity().setResult(RESULT_OK,i2);
+                getActivity().finish();
+
 
             }
         });
--- app/src/main/java/com/example/jp/comunicacionfragmentser/MainActivityFragment.java
+++ app/src/main/java/com/example/jp/comunicacionfragmentser/MainActivityFragment.java
@@ -16,13 +16,13 @@ import static android.app.Activity.RESULT_OK;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-
+    Button boton;
+    TextView texto1;
+    public static final int CODE = 1;// Codigo de solicitud
     public MainActivityFragment() {
     }
 
-    Button boton;
-    TextView texto1;
-    static final int CODE = 1;// Codigo de solicitud
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,17 +31,17 @@ public class MainActivityFragment extends Fragment {
         View vista = inflater.inflate(R.layout.fragment_main, container, false);
 
         boton = (Button) vista.findViewById(R.id.bplay);
-        texto1=(TextView)vista.findViewById(R.id.tv1);
+        texto1 = (TextView) vista.findViewById(R.id.tv1);
 
         boton.setOnClickListener(new View.OnClickListener() {
-            @Override
+
             public void onClick(View v) {
 
                 Intent i = new Intent(getActivity(), Main2Activity.class);
-                i.putExtra("Hola","Activity2");
+                i.putExtra("llamarActivity1", "Hola Activity2");
 
 
-                startActivityForResult(i,CODE          );
+                startActivityForResult(i, CODE);
             }
         });
 
@@ -49,17 +49,20 @@ public class MainActivityFragment extends Fragment {
 
 
     }
-
+@Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+
         if (requestCode == CODE) {
 
             if (resultCode == RESULT_OK) {
 
-                String result=data.getStringExtra("llamar2");
+                String result = data.getStringExtra("LlamarActivity2");
                 texto1.setText(result);
 
             }
 
 
         }
+    }
 }
