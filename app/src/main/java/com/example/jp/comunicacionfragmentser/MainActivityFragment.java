ea6b20d0146c2841eaec7996a538a61b8db763ff
19julio94 <juliopatriciodasilva@gmail.com>
2017-01-17 08:48:43 +0100

    Boton para pasar al segundo activity listo y funcionando

--- app/src/main/java/com/example/jp/comunicacionfragmentser/MainActivityFragment.java
+++ app/src/main/java/com/example/jp/comunicacionfragmentser/MainActivityFragment.java
@@ -1,10 +1,13 @@
 package com.example.jp.comunicacionfragmentser;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,9 +17,29 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
     }
 
+    Button boton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View vista=inflater.inflate(R.layout.fragment_main, container, false);
+
+        boton = (Button)vista.findViewById(R.id.bplay);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i=new Intent(getActivity(),Main2Activity.class);
+
+                Toast.makeText(getActivity(), "Activity 2", Toast.LENGTH_SHORT).show();
+
+                startActivity(i);
+            }
+        });
+
+        return vista;
+
+
     }
 }
